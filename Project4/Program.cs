f8bfdd7ed61ce3315264206e8980673906eaa1e3
Cornelius Donley <donley@live.com>
2016-05-10 15:58:29 -0500

    Init

--- Project4/Program.cs
+++ Project4/Program.cs
@@ -1,15 +1,268 @@
-﻿using System;
+﻿/*
+ * Cornelius Donley 
+ * Project 4
+ * Program.cs
+ * This program animates the Insertion Sort and Quicksort algorithms
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+
 
 namespace Project4
 {
     class Program
     {
+        [STAThread]
         static void Main(string[] args)
         {
+            Application app = new Application();
+            app.Run(new SortingWindow());
         }
+    }
+
+    class SortingWindow : Window
+    {
+        //Globals  
+        const int NUM_ALGOS = 2;
+        const double WIDTH = 800.0, HEIGHT = 600.0;
+        enum MenuItems { ArraySetUp, AnimationType };
+        enum AnimationButtons { Start, Pause, Manual };
+        enum SortingAlgorithms { InsertionSort, Quicksort };
+        string[] sortingOptions = { "Insertion Sort", "Quicksort" };
+        MenuItem[] menuItems = new MenuItem[2];
+        Button[] animationButtons = new Button[3];
+        GroupBox[] sortingGroupBoxes = new GroupBox[NUM_ALGOS];
+        DispatcherTimer timer = new DispatcherTimer();
+        InsertionSort insertionSort;
+        Quicksort quicksort;
+
+        public SortingWindow()
+        {
+            //Variables            
+            var dockPanel = new DockPanel();
+            var menuStackPanel = new StackPanel();
+            var centerStackPanel = new StackPanel();
+            var buttonStackPanel = new StackPanel();
+            var mainMenu = new Menu();
+
+            menuStackPanel.Orientation = Orientation.Horizontal;            
+            buttonStackPanel.Orientation = Orientation.Horizontal;
+            buttonStackPanel.VerticalAlignment = VerticalAlignment.Bottom;
+            dockPanel.Children.Add(buttonStackPanel);
+            dockPanel.Children.Add(menuStackPanel);
+            dockPanel.Children.Add(centerStackPanel);
+            DockPanel.SetDock(menuStackPanel, Dock.Top);
+            DockPanel.SetDock(buttonStackPanel, Dock.Bottom);
+            this.Width = WIDTH;
+            this.Height = HEIGHT * 0.33;
+            this.Content = dockPanel;
+            this.Title = "Donley's Sorting Animations";
+
+            //Main Menu
+            for (int i = 0; i < menuItems.Length; i++)
+                menuItems[i] = new MenuItem();  
+            menuStackPanel.Children.Add(mainMenu);
+            menuStackPanel.Background = mainMenu.Background;
+            menuItems[(int)MenuItems.ArraySetUp].Header = "Array Set Up";
+            menuItems[(int)MenuItems.AnimationType].Header = "Animation Type";
+            menuItems[(int)MenuItems.ArraySetUp].Click += ArraySetupMenu_Click;
+            foreach (MenuItem m in menuItems) 
+            {
+                m.Margin = new Thickness(5, 5, 10, 5);
+                m.Padding = new Thickness(5);
+                m.VerticalAlignment = VerticalAlignment.Center;
+                m.Background = Brushes.LightGray;
+                mainMenu.Items.Add(m);
+            }
+
+            //Sorting Types Submenu
+            for (int i = 0; i < NUM_ALGOS; i++)
+            {
+                var sortingMenuItem = new MenuItem();
+                sortingMenuItem.Header = sortingOptions[i];
+                sortingMenuItem.Tag = (SortingAlgorithms)i;
+                sortingMenuItem.IsCheckable = true;
+                sortingMenuItem.StaysOpenOnClick = true;
+                sortingMenuItem.Click += SortingMenuItem_Click;
+                menuItems[(int)MenuItems.AnimationType].Items.Add(sortingMenuItem);
+            }
+
+            //Group Boxes
+            for (int i = 0; i < NUM_ALGOS; i++)
+            {
+                sortingGroupBoxes[i] = new GroupBox();
+                sortingGroupBoxes[i].Header = sortingOptions[i];
+                sortingGroupBoxes[i].Margin = new Thickness(5, 10, 5, 10);
+                sortingGroupBoxes[i].Height = 0;
+                sortingGroupBoxes[i].Visibility = Visibility.Hidden;
+                centerStackPanel.Children.Add(sortingGroupBoxes[i]);
+            }
+
+            //Buttons      
+            for (int i = 0; i < animationButtons.Length; i++)
+                animationButtons[i] = new Button();                
+            animationButtons[(int)AnimationButtons.Start].Content = "Start/Continue Animation";
+            animationButtons[(int)AnimationButtons.Pause].Content = "Pause Animtion";
+            animationButtons[(int)AnimationButtons.Manual].Content = "Manual Step";
+            for (int i = 0; i < animationButtons.Length; i++)
+            {
+                animationButtons[i].Tag = (AnimationButtons)i;
+                animationButtons[i].Width = 200.0;
+                animationButtons[i].Margin = new Thickness(20, 10, 20, 10);
+                animationButtons[i].Padding = new Thickness(5);
+                animationButtons[i].Click += AnimationButton_Click;                
+                buttonStackPanel.Children.Add(animationButtons[i]);
+            }
+
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 1);
+            Reset();
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            var numChecked = NUM_ALGOS;
+            var numDone = 0;
+            foreach (MenuItem m in menuItems[(int)MenuItems.AnimationType].Items)
+            {
+                if ((SortingAlgorithms)m.Tag == SortingAlgorithms.InsertionSort && m.IsChecked)
+                    if (!insertionSort.IsDone) insertionSort.Sort();
+                    else numDone++;
+                if ((SortingAlgorithms)m.Tag == SortingAlgorithms.InsertionSort && !m.IsChecked) numChecked--;
+                if ((SortingAlgorithms)m.Tag == SortingAlgorithms.Quicksort && m.IsChecked)
+                    if (!quicksort.IsDone) quicksort.Sort();
+                    else numDone++;
+                if ((SortingAlgorithms)m.Tag == SortingAlgorithms.Quicksort && !m.IsChecked) numChecked--;
+            }
+            if (numChecked == numDone) Reset();
+        }
+
+        private void AnimationButton_Click(object sender, RoutedEventArgs e)
+        {
+            var btn = sender as Button;
+            switch((AnimationButtons)btn.Tag)
+            {
+                case AnimationButtons.Start:
+                    menuItems[(int)MenuItems.AnimationType].IsEnabled = false;
+                    animationButtons[(int)AnimationButtons.Start].IsEnabled = false;
+                    animationButtons[(int)AnimationButtons.Pause].IsEnabled = true;
+                    animationButtons[(int)AnimationButtons.Manual].IsEnabled = false;
+                    timer.Start();
+                    break;
+                case AnimationButtons.Manual:
+                    menuItems[(int)MenuItems.AnimationType].IsEnabled = false;
+                    Timer_Tick(sender, e);
+                    break;
+                case AnimationButtons.Pause:
+                    animationButtons[(int)AnimationButtons.Start].IsEnabled = true;
+                    animationButtons[(int)AnimationButtons.Pause].IsEnabled = false;
+                    animationButtons[(int)AnimationButtons.Manual].IsEnabled = true;
+                    timer.Stop();
+                    break;
+            }
+        }
+
+        private void ArraySetupMenu_Click(object sender, RoutedEventArgs e)
+        {
+            var size = DialogBox("New Random Array", "Enter size of the array (2-50):");
+            if (size < 2 || size > 50) return;
+            var arr = PopulateArray(size);
+            insertionSort = new InsertionSort(arr);
+            insertionSort.Height = HEIGHT * 0.33;
+            insertionSort.Width = WIDTH * 0.95;
+            insertionSort.DrawBarGraph();
+            sortingGroupBoxes[(int)SortingAlgorithms.InsertionSort].Content = insertionSort.Canvas;
+            quicksort = new Quicksort(arr);
+            quicksort.Height = HEIGHT * 0.33;
+            quicksort.Width = WIDTH * 0.95;
+            quicksort.DrawBarGraph();    
+            sortingGroupBoxes[(int)SortingAlgorithms.Quicksort].Content = quicksort.Canvas;
+            menuItems[(int)MenuItems.AnimationType].IsEnabled = true;
+            var m = sender as MenuItem;
+            m.IsEnabled = false;
+            bool b = false;
+            foreach (MenuItem mm in menuItems[(int)MenuItems.AnimationType].Items)
+                if (mm.IsChecked) b = true;
+            animationButtons[(int)AnimationButtons.Start].IsEnabled = b;
+            animationButtons[(int)AnimationButtons.Manual].IsEnabled = b;
+        }
+
+        private void SortingMenuItem_Click(object sender, RoutedEventArgs e)
+        {            
+            var m = sender as MenuItem;
+            bool b = false;
+            foreach (MenuItem mm in menuItems[(int)MenuItems.AnimationType].Items)
+                if (mm.IsChecked) b = true;
+            animationButtons[(int)AnimationButtons.Start].IsEnabled = b;
+            animationButtons[(int)AnimationButtons.Manual].IsEnabled = b;
+            switch ((SortingAlgorithms)m.Tag)
+            {
+                case SortingAlgorithms.InsertionSort:
+                    if (m.IsChecked)
+                    {
+                        sortingGroupBoxes[(int)SortingAlgorithms.InsertionSort].Visibility = Visibility.Visible;
+                        sortingGroupBoxes[(int)SortingAlgorithms.InsertionSort].Height = HEIGHT * 0.37;
+                        this.Height += HEIGHT * 0.34;
+                    }
+                    else
+                    {
+                        sortingGroupBoxes[(int)SortingAlgorithms.InsertionSort].Visibility = Visibility.Hidden;
+                        sortingGroupBoxes[(int)SortingAlgorithms.InsertionSort].Height = 0;
+                        this.Height -= HEIGHT * 0.34;
+                    }
+                    break;
+                case SortingAlgorithms.Quicksort:
+                    if (m.IsChecked)
+                    {
+                        sortingGroupBoxes[(int)SortingAlgorithms.Quicksort].Visibility = Visibility.Visible;
+                        sortingGroupBoxes[(int)SortingAlgorithms.Quicksort].Height = HEIGHT * 0.37;
+                        this.Height += HEIGHT * 0.34;
+                    }
+                    else
+                    {
+                        sortingGroupBoxes[(int)SortingAlgorithms.Quicksort].Visibility = Visibility.Hidden;
+                        sortingGroupBoxes[(int)SortingAlgorithms.Quicksort].Height = 0;
+                        this.Height -= HEIGHT * 0.34;
+                    }
+                    break;
+            }
+        }
+
+        void Reset()
+        {
+            menuItems[(int)MenuItems.ArraySetUp].IsEnabled = true;
+            menuItems[(int)MenuItems.AnimationType].IsEnabled = false;            
+            animationButtons[(int)AnimationButtons.Start].IsEnabled = false;
+            animationButtons[(int)AnimationButtons.Pause].IsEnabled = false;
+            animationButtons[(int)AnimationButtons.Manual].IsEnabled = false;
+            timer.Stop();
+        }
+
+        double [] PopulateArray(int size)
+        {
+            var rand = new Random();
+            var arr = new double[size];
+            for (int i = 0; i < size; i++)
+                arr[i] = rand.NextDouble();
+            return arr;
+        }
+
+        //Creates dialog box with default value of 10
+        int DialogBox(string title, string text, string defaultText = "10")
+        {
+            var size = Microsoft.VisualBasic.Interaction.InputBox(text, title, defaultText);
+            int n;
+            if (!int.TryParse(size, out n)) n = 0;
+            return n;
+        }
+        
     }
 }
---
+++ Project4/SortingAnimation.cs
@@ -0,0 +1,187 @@
+﻿/*
+ * Cornelius Donley 
+ * Project 4
+ * SortingAnimation.cs
+ * This program animates the Insertion Sort and Quicksort algorithms
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Project4
+{
+    abstract class SortingAnimation
+    {
+        protected double[] array;
+        protected Label[] colorBars;
+        Random rand;
+
+        public Canvas Canvas { get; private set; }    
+        public bool IsDone { get; protected set; }    
+        public double Height { private get { return this.Canvas.Height; } set { this.Canvas.Height = value; } }   
+        public double Width { private get { return this.Canvas.Width; } set { this.Canvas.Width = value; } }
+
+        public SortingAnimation(double[] arr, int seed = 13)
+        {
+            this.Canvas = new Canvas();
+            rand = new Random(seed);
+            array = arr;
+            colorBars = new Label[array.Length];
+            for (int i = 0; i < colorBars.Length; i++)
+            {
+                colorBars[i] = new Label();
+                colorBars[i].BorderThickness = new Thickness(2);
+                colorBars[i].BorderBrush = Brushes.Black;
+                colorBars[i].Tag = i;
+
+                var R = rand.Next(256);
+                var G = rand.Next(256);
+                var B = rand.Next(256);
+                colorBars[i].Background = new SolidColorBrush(Color.FromRgb((byte)R, (byte)G, (byte)B));
+
+                Canvas.SetBottom(colorBars[i], 0);
+                this.Canvas.Children.Add(colorBars[i]);
+            }
+        }
+
+        public void DrawBarGraph()
+        {
+            for (int i = 0; i < colorBars.Length; i++)
+            {
+                colorBars[i].Height = this.Canvas.Height * array[(int)colorBars[i].Tag];
+                colorBars[i].Width = this.Canvas.Width / colorBars.Length;
+                Canvas.SetLeft(colorBars[i], this.Canvas.Width / colorBars.Length * i);
+            }
+        }
+
+        public abstract void Sort();
+    }
+
+    class InsertionSort : SortingAnimation
+    {
+        int i = 1, k = 1;
+
+        public InsertionSort(double[] arr, int seed = 13) : base(arr, seed)
+        {
+
+        }
+
+        public override void Sort()
+        {
+            if(k == colorBars.Length)
+            {
+                IsDone = true;
+                return;
+            }
+            if(k == 0 || array[(int)colorBars[k].Tag] >= array[(int)colorBars[k - 1].Tag])
+            {
+                i++;
+                k = i;
+                return;
+            }
+            if (array[(int)colorBars[k].Tag] < array[(int)colorBars[k - 1].Tag])
+            {
+                object tempTag = colorBars[k].Tag;
+                Brush tempBrush = colorBars[k].Background;
+                colorBars[k].Tag = colorBars[k - 1].Tag;
+                colorBars[k].Background = colorBars[k - 1].Background;
+                colorBars[k - 1].Tag = tempTag;
+                colorBars[k - 1].Background = tempBrush;
+                colorBars[k].Height = this.Canvas.Height * array[(int)colorBars[k].Tag];
+                colorBars[k - 1].Height = this.Canvas.Height * array[(int)colorBars[k - 1].Tag];
+                k--;
+            }
+        }
+    }
+
+    class Quicksort : SortingAnimation
+    {
+        Stack<Range> stack;
+        Range range;
+        bool done;
+        int lower, upper;
+
+        class Range
+        {
+            public Range(int lower, int pivot)
+            {
+                Lower = lower;
+                Pivot = pivot;
+            }
+            public int Lower { get; set; }
+            public int Pivot { get; set; }
+            public double Value { get; set; }
+        }
+
+        public Quicksort(double[] arr, int seed = 13) : base(arr, seed)
+        {
+            stack = new Stack<Range>();
+            range = new Range(0, array.Length - 1);
+            lower = range.Lower;
+            upper = range.Pivot - 1;
+            range.Value = array[(int)colorBars[range.Pivot].Tag];
+            done = false;
+        }
+
+        public override void Sort()
+        {
+            if (!done) Partition();
+            else if (stack.Count > 0)
+            {
+                range = stack.Pop();
+                lower = range.Lower;
+                upper = range.Pivot - 1;
+                range.Value = array[(int)colorBars[range.Pivot].Tag];
+                Partition();
+            }
+            
+        }
+
+        private void Partition()
+        {
+            //if (lower > range.Pivot) return;
+            while (lower < range.Pivot && array[(int)colorBars[lower].Tag] < array[(int)colorBars[range.Pivot].Tag])
+            {
+                lower++;
+            }
+            if (lower < upper)
+            {
+                Swap(lower, upper);
+                upper--;
+                done = false;
+            }
+            else
+            {
+                Swap(lower, range.Pivot);
+                done = true;
+                if (lower + 1 < range.Pivot)
+                {
+                    stack.Push(new Range(lower + 1, range.Pivot));
+                }
+                if (lower - 1 > range.Lower)
+                {
+                    stack.Push(new Range(range.Lower, lower - 1));
+                }
+                if (stack.Count == 0) IsDone = true;
+            }
+        }
+
+        private void Swap(int a, int b)
+        {
+            var t = colorBars[a].Tag;
+            var bg = colorBars[a].Background;
+            colorBars[a].Tag = colorBars[b].Tag;
+            colorBars[a].Background = colorBars[b].Background;
+            colorBars[a].Height = this.Canvas.Height * array[(int)colorBars[a].Tag];
+            colorBars[b].Tag = t;
+            colorBars[b].Background = bg;
+            colorBars[b].Height = this.Canvas.Height * array[(int)colorBars[b].Tag];
+        }
+    }
+}
